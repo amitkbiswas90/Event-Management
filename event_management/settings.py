e185617b896e16755dc6f72894a45b804b09aebc
amitkbiswas90 <amitkbiswas90@gmail.com>
2025-02-03 17:17:52 +0600

    databse setup done

--- event_management/settings.py
+++ event_management/settings.py
@@ -1,4 +1,4 @@
-
+import dj_database_url
 
 from pathlib import Path
 
@@ -72,15 +72,24 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'event_management',
+#         'USER': 'postgres',
+#         'PASSWORD': 'amit',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event_management',
-        'USER': 'postgres',
-        'PASSWORD': 'amit',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_management_db_8cu3_user:Yuz5285luxYhUIHRBKfAqSOrWL5xcT1c@dpg-cuga1ldds78s738dqlo0-a.oregon-postgres.render.com/event_management_db_8cu3',
+        conn_max_age=600
+    )
 }
 
 
